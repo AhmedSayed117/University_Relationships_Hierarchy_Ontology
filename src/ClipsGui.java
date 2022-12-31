00af6e2af5ada403d39c96919b169c4007e6bb9b
Ahmed Sayed <midosayed1762001@gmail.com>
2022-12-31 18:26:22 +0200

    final

--- src/ClipsGui.java
+++ src/ClipsGui.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClipsGui extends JFrame{
@@ -74,7 +73,7 @@ public class ClipsGui extends JFrame{
                 if (B1.isSelected()){
                     p1 = new popup("Department name","Enter Department Name To get All TAs ");
                     if (o1!=null) o1.dispose();
-                }
+                }else p1 = null;
             }
         });
 
@@ -84,7 +83,7 @@ public class ClipsGui extends JFrame{
                 if (B2.isSelected()){
                     p2 = new popup("Student name","Enter Student name To get All courses ");
                     if (o2!=null) o2.dispose();
-                }
+                }else p2 = null;
             }
         });
 
@@ -94,7 +93,7 @@ public class ClipsGui extends JFrame{
                 if (B3.isSelected()){
                     p3 = new popup("Faculty name","Enter Faculty name To get all courses");
                     if (o3!=null) o3.dispose();
-                }
+                }else p3 = null;
             }
         });
 
@@ -104,7 +103,7 @@ public class ClipsGui extends JFrame{
                 if (B4.isSelected()){
                     p4 = new popup("Faculty name","Enter Faculty name to get All Lecturers ");
                     if (o4!=null) o4.dispose();
-                }
+                }else p4 = null;
             }
         });
 
@@ -114,7 +113,7 @@ public class ClipsGui extends JFrame{
                 if (B5.isSelected()){
                     p5 = new popup("University name","Enter University name To get All faculties ");
                     if (o5!=null) o5.dispose();
-                }
+                }else p5 = null;
             }
         });
 
@@ -128,49 +127,48 @@ public class ClipsGui extends JFrame{
                 clips.reset();
 
                 if (p1!=null && !p1.ReturnInput().isEmpty()){
-                    clips.assertString("(dep " + p1.ReturnInput()  + ")");
-                    clearFile("GUI1.txt");
+                    clips.assertString("(dep " + p1.ReturnInput().toUpperCase()  + ")");
+                    clearFile("TEMP/GUI1.txt");
                 }
                 if (p2!=null && !p2.ReturnInput().isEmpty()){
                     clips.assertString("(stu " + p2.ReturnInput()  + ")");
-                    clearFile("GUI2.txt");
-
+                    clearFile("TEMP/GUI2.txt");
                 }
                 if (p3!=null && !p3.ReturnInput().isEmpty()){
                     clips.assertString("(Fac " +"\""+ p3.ReturnInput() +"\"" + ")");
-                    clearFile("GUI3.txt");
+                    clearFile("TEMP/GUI3.txt");
                 }
                 if (p4!=null && !p4.ReturnInput().isEmpty()){
                     clips.assertString("(FacL " +"\""+ p4.ReturnInput() +"\"" + ")");
-                    clearFile("GUI4.txt");
+                    clearFile("TEMP/GUI4.txt");
                 }
                 if (p5!=null && !p5.ReturnInput().isEmpty()){
                     clips.assertString("(University " +"\""+ p5.ReturnInput() +"\"" + ")");
-                    clearFile("GUI5.txt");
+                    clearFile("TEMP/GUI5.txt");
                 }
-                if (p1!=null && p2!=null && p3!=null && p4!=null && p5!=null && !p1.ReturnInput().isEmpty()&& !p2.ReturnInput().isEmpty()&& !p3.ReturnInput().isEmpty()&& !p4.ReturnInput().isEmpty()&& !p5.ReturnInput().isEmpty()){
-                    clips.run();
 
-                    String t = readfile("GUI1.txt");
-                    o1 = new popupOutput("All TAs in "+ p1.ReturnInput() +" Department");
+                clips.run();
+
+                if (p1!=null && p1.ReturnInput()!=null) {
+                    String t = readfile("TEMP/GUI1.txt");
+                    o1 = new popupOutput("All TAs in " + p1.ReturnInput() + " Department");
                     o1.SetText(t);
-
-                    String t2 = readfile("GUI2.txt");
+                }if(p2!=null && p2.ReturnInput()!=null){
+                    String t2 = readfile("TEMP/GUI2.txt");
                     o2 = new popupOutput("All courses that "+p2.ReturnInput()+ " currently studying");
                     o2.SetText(t2);
-
-                    String t3 = readfile("GUI3.txt");
+                }if(p3!=null && p3.ReturnInput()!=null){
+                    String t3 = readfile("TEMP/GUI3.txt");
                     o3 = new popupOutput("All courses offered by "+p3.ReturnInput());
                     o3.SetText(t3);
-
-                    String t4 = readfile("GUI4.txt");
+                }if(p4!=null && p4.ReturnInput()!=null){
+                    String t4 = readfile("TEMP/GUI4.txt");
                     o4 = new popupOutput("All Lecturers in "+p4.ReturnInput());
                     o4.SetText(t4);
-
-                    String t5 = readfile("GUI5.txt");
+                }if(p5!=null && p5.ReturnInput()!=null){
+                    String t5 = readfile("TEMP/GUI5.txt");
                     o5 = new popupOutput("All faculties in "+p5.ReturnInput());
                     o5.SetText(t5);
-
                 }
             }
         });
